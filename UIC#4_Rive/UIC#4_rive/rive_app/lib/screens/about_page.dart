3918ed7b745d6bfe85f30e88a00bcd967f080e26
satyamraj <satyamraj-creator@users.noreply.github.com>
2020-04-19 18:15:24 +0530

    updated
    Added minion

--- UIC#4_Rive/UIC#4_rive/rive_app/lib/screens/about_page.dart
+++ UIC#4_Rive/UIC#4_rive/rive_app/lib/screens/about_page.dart
@@ -1,9 +1,45 @@
+import 'package:flare_flutter/flare_actor.dart';
 import 'package:flutter/material.dart';
+import 'package:flare_flutter/flare_controls.dart';
+
 class AbouPage extends StatelessWidget {
+  final FlareControls animationControls = FlareControls();
   @override
   Widget build(BuildContext context) {
-    return Container(child:
-      Center(child: Text('Follow Me On GitHub @satyamraj123 For More!',style: Theme.of(context).textTheme.title,),)
+    return Container(
+      child: Column(
+        children: <Widget>[
+          Text(
+            'Follow Me On GitHub @satyamraj123 For More!',
+            style: Theme.of(context).textTheme.title,
+          ),
+          GestureDetector(
+            onTap: (){
+            animationControls.play('Wave');
+            animationControls.play('Stand');
+            },
+            onVerticalDragStart: (_){
+              animationControls.play('Jump');
+              animationControls.play('Stand');
+            },
+            
+            onHorizontalDragStart: (_){
+              animationControls.play('Dance');
+              animationControls.play('Stand');
+            },
+                      child: Container(
+                height: 300,
+                width: MediaQuery.of(context).size.width,
+                alignment: Alignment.topCenter,
+                child: FlareActor(
+                  'assets/Bob.flr',
+                  animation: 'Stand',
+                  fit: BoxFit.cover,
+                  controller: animationControls,
+                )),
+          )
+        ],
+      ),
     );
   }
 }
